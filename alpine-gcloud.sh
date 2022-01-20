2f6fbd111583318e0902be9bdb1202c0cd7881c4
Mauricio Santana Goncalves <mgoncalves@golesuite.com>
2022-01-20 08:24:08 -0300

    create three functions. Organization

--- alpine-gcloud.sh
+++ alpine-gcloud.sh
@@ -43,6 +43,8 @@ elif ! [[ "$SCHEDULER_LABEL" =~ "=" ]]; then
     exit 1
 fi
 
+set -x
+
 apk add --no-cache curl python3
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-368.0.0-linux-x86_64.tar.gz
 tar xzf google-cloud-sdk-368.0.0-linux-x86_64.tar.gz
@@ -58,25 +60,42 @@ mv kubectl /usr/bin
                                  --internal-ip \
                                  --zone $GCLOUD_ZONE --project $PROJECT_ID
 
-set -x
+resize_cluster(){
+    /opt/google-cloud-sdk/bin/gcloud container clusters resize -q $CLUSTER_NAME \
+                                     --node-pool $SCHEDULER_POOL --num-nodes $SCALE_NODES_NUMBER \
+                                     --zone $GCLOUD_ZONE --project $PROJECT_ID
+}
 
-ALL_DEPLOY=($(kubectl get -A -l "$SCHEDULER_LABEL" deploy -o=jsonpath='{.items[*].metadata.namespace}' | tr " " "\n" | sort | uniq ))
+resize_deploys(){
+    ALL_DEPLOY=($(kubectl get -A -l "$SCHEDULER_LABEL" deploy -o=jsonpath='{.items[*].metadata.namespace}' | tr " " "\n" | sort | uniq ))
+    if test "${#ALL_DEPLOY}" -gt 0; then
+        NS=($(echo "${ALL_DEPLOY[*]}" | sort | uniq | cut -d":" -f 1))
+        for i in "${NS[*]}"; do
+            kubectl -n "$i" scale deploy -l $SCHEDULER_LABEL --replicas=${SCALE_DEPLOY_NUMBER}
+        done
+    fi
+}
 
-if test "${#ALL_DEPLOY}" -gt 0; then
-    NS=($(echo "${ALL_DEPLOY[*]}" | sort | uniq | cut -d":" -f 1))
-    for i in "${NS[*]}"; do
-        kubectl -n "$i" scale deploy -l $SCHEDULER_LABEL --replicas=${SCALE_DEPLOY_NUMBER}
-    done
+resize_sts(){
+    STS=($(kubectl get -A -l "$SCHEDULER_LABEL" sts -o=jsonpath='{.items[*].metadata.namespace}' | sort | uniq ))
+    if test "${#ALL_STS}" -gt 0; then
+        NS=($(echo "${ALL_STS[*]}" | cut -d":" -f 1))
+        for i in "${NS[*]}"; do
+            kubectl -n $i scale sts -l $SCHEDULER_LABEL --replicas=${SCALE_STS_NUMBER}
+        done
+    fi
+}
+
+# first resize cluster then create pods
+if test "$SCALE_NODES_NUMBER" -eq "0"; then
+    resize_cluster;
+    sleep 60;
+    resize_deploys;
+    resize_sts;
+    RESIZE="true"
+else
+    resize_deploys;
+    resize_sts;
+    sleep 60;
+    resize_cluster;
 fi
-
-ALL_STS=($(kubectl get -A -l "$SCHEDULER_LABEL" sts -o=jsonpath='{.items[*].metadata.namespace}' | sort | uniq ))
-if test "${#ALL_STS}" -gt 0; then
-    NS=($(echo "${ALL_STS[*]}" | cut -d":" -f 1))
-    for i in "${NS[*]}"; do
-        kubectl -n $i scale sts -l $SCHEDULER_LABEL --replicas=${SCALE_STS_NUMBER}
-    done
-fi
-
-/opt/google-cloud-sdk/bin/gcloud container clusters resize -q $CLUSTER_NAME \
-                                 --node-pool $SCHEDULER_POOL --num-nodes $SCALE_NODES_NUMBER \
-                                 --zone $GCLOUD_ZONE --project $PROJECT_ID
