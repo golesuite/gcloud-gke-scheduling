ce29458e83747b2238c1f05de6c79fd55ce6a677
Mauricio Santana Goncalves <mgoncalves@golesuite.com>
2022-12-20 22:46:25 -0300

    add gcloud on path

--- alpine-gcloud.sh
+++ alpine-gcloud.sh
@@ -52,6 +52,7 @@ mv google-cloud-sdk /opt
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv kubectl /usr/bin
+ln -s /opt/google-cloud-sdk/bin/gcloud /usr/local/bin/
 
 # gcloud auth
 /opt/google-cloud-sdk/bin/gcloud components install gke-gcloud-auth-plugin
@@ -95,7 +96,6 @@ if test "$SCALE_NODES_NUMBER" -eq "0"; then
     resize_cluster;
 else
     resize_cluster;
-    sleep 60;
     resize_deploys;
     resize_sts;
 fi
